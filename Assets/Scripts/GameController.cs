c45495748a0a0ee3a96c31d86872a76db24504f9
Skyz011 <89059430+Skyz011@users.noreply.github.com>
2023-11-20 17:41:12 -0500

    Init

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public Text scoreText;
+    int score = 0;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
